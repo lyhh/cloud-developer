98534da61eb72afe501dd84fe4f6482996c769b4
Ho, Ly Huong <huonglyho89@gmail.com>
2023-04-23 11:44:41 +0800

    Fix 'Could not find MIME for Buffer <null>' error

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import fetch from "node-fetch";
 import Jimp = require("jimp");
 
 // filterImageFromURL
@@ -11,7 +12,9 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      // Turn the image fetched from input url to a buffer and then to a Jimp image, otherwise some images are not processed
+      const fetchedPhoto = await fetch(inputURL);
+      const photo = await Jimp.read(await fetchedPhoto.arrayBuffer());
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
